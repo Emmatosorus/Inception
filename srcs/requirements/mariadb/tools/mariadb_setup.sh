309c896688170f39397046756b8f12ff698a7458
Emma <emma@bouteyres.com>
2024-11-27 17:07:24 +0100

    Remove 💥: Unessassery comments

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Start MariaDB in background
 mysqld_safe --skip-networking &
 sleep 5
 
-# Run initialization commands
 mysql -u root <<-EOSQL
     CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;
     CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';
@@ -13,8 +11,6 @@ mysql -u root <<-EOSQL
     FLUSH PRIVILEGES;
 EOSQL
 
-# Stop background service
 mysqladmin --user=root --password=${SQL_ROOT_PASSWORD} shutdown
 
-# Start MariaDB in foreground
 exec mysqld --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# Wait for MariaDB
 while ! mysqladmin ping -h"mariadb" --silent; do
     echo "Waiting for MariaDB..."
-    sleep 3
+    sleep 2
 done
 
 cd /var/www/html
 
-# Clean install if needed
 if [ ! -f "wp-config.php" ] || [ ! -d "wp-content" ]; then
     rm -rf *
     
-    # Install wp-cli
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
     chmod +x wp-cli.phar
     mv wp-cli.phar /usr/local/bin/wp
 
-    # Install WordPress
     wp core download --version=${WORDPRESS_VERSION} --allow-root
     
-    # Create wp-config.php
     wp config create \
         --dbname=${SQL_DATABASE} \
         --dbuser=${SQL_USER} \
@@ -28,7 +23,6 @@ if [ ! -f "wp-config.php" ] || [ ! -d "wp-content" ]; then
         --dbhost=mariadb:3306 \
         --allow-root
     
-    # Install WordPress core
     wp core install \
         --url=https://epolitze.42.fr \
         --title="Inception" \
